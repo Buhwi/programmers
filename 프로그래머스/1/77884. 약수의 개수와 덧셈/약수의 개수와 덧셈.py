18b34ea33b45a76fb4f88e87091bb98a8a4dc2ae
Buhwi <149663256+Buhwi@users.noreply.github.com>
2024-07-11 16:17:33 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.40 ms, Memory: 9.92 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -0,0 +1,14 @@
+def solution(left, right):
+    answer = 0
+    n = 0
+    for i in range(left, right+1):
+        for j in range(1, i+1):
+            if (i % j) == 0:
+                n += 1
+        if(n%2) == 0:
+            answer += i
+        else:
+            answer -= i
+        n = 0
+        
+    return answer
